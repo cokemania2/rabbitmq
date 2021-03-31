3b6aaa419dc9030ba2adc732be27ca1595220e5d
coke <coke@mailplug.co.kr>
2021-03-31 14:56:25 +0900

    updtae

--- run_tasks.py
+++ run_tasks.py
@@ -4,6 +4,6 @@ import kombu
 
 if __name__ == '__main__':
     for i in range(30) :
-        result = longtime_add.apply_async((1,2), queue='moon')
-        result2 = longtime_mul.apply_async((1,2), queue='sunshine')
-        result3 = longtime_ok.apply_async((0,0), queue='celery')
+        result = longtime_add.apply_async((1,2))
+        result2 = longtime_mul.apply_async((1,2))
+        result3 = longtime_ok.apply_async((0,0))
--- tasks2.py
+++ tasks2.py
@@ -3,48 +3,13 @@ from kombu import Exchange, Queue
 from celery import Celery
 import time
 
-default_exchange_name = 'celery'
-
-default_queue_name = 'celery'
-default_routing_key = 'celery'
-
-sunshine_queue_name = 'sunshine'
-sunshine_routing_key = 'sunshine'
-
-moon_queue_name = 'moon'
-moon_routing_key = 'moon'
-
 app = Celery('rabbitmq',
              broker='amqp://test:test@localhost/',
-             routing_key=default_routing_key,
              include=['tasks2'])
-
-default_exchange = Exchange(default_exchange_name, type='direct')
-default_queue = Queue(
-    default_queue_name,
-    default_exchange,
-    routing_key=default_routing_key)
-
-sunshine_queue = Queue(
-    sunshine_queue_name,
-    default_exchange,
-    routing_key=sunshine_routing_key)
-
-moon_queue = Queue(
-    moon_queue_name,
-    default_exchange,
-    routing_key=moon_queue_name)
-
-app.conf.task_queues = (default_queue, sunshine_queue, moon_queue)
-
-app.conf.task_default_queue = default_queue_name
-app.conf.task_default_exchange = default_exchange_name
-app.conf.task_default_routing_key = default_routing_key
 
 @app.task
 def longtime_add(x, y):
     print ('long time task begins')
-    # sleep 5 seconds
     time.sleep(5)
     print ('long time task finished')
     return x + y
@@ -52,7 +17,6 @@ def longtime_add(x, y):
 @app.task
 def longtime_mul(x, y) :
     print ('long time task begins')
-    # sleep 5 seconds
     time.sleep(5)
     print ('long time task finished')
     return x * y
@@ -60,7 +24,6 @@ def longtime_mul(x, y) :
 @app.task
 def longtime_ok(x, y) :
     print ('long time OK begins')
-    # sleep 5 seconds
     time.sleep(5)
     print ('long time OK finished')
     return x + y
